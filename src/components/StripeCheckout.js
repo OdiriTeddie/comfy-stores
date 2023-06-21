5181b5e7c7933d8376efce26f598b66e33d1d814
Odiri Teddie <odiriteddie.adamu@gmail.com>
2023-06-20 22:47:38 -0400

    completing stripe payment

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,6 +12,7 @@ import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
 import { formatPrice } from "../utils/helpers";
 import { useNavigate } from "react-router-dom";
+import { findAllByTestId } from "@testing-library/react";
 
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
@@ -21,13 +22,13 @@ const CheckoutForm = () => {
   const navigate = useNavigate();
   // STRIPE STUFF
 
-  const [succeeded, setSucceeded] = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
   const [error, setError] = useState(null);
   const [processing, setProcessing] = useState("");
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState("");
   const stripe = useStripe();
-  const elemetns = useElements();
+  const elements = useElements();
 
   const cardStyle = {
     style: {
@@ -49,11 +50,14 @@ const CheckoutForm = () => {
 
   const createPaymentIntent = async () => {
     try {
-      const data = await axios.post(
+      const { data } = await axios.post(
         "/.netlify/functions/create-payment-intent",
         JSON.stringify({ cart, shipping_fee, total_amount })
       );
-    } catch (error) {}
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      // console.log(error.response);
+    }
   };
 
   useEffect(() => {
@@ -61,11 +65,45 @@ const CheckoutForm = () => {
     // eslint-disable-next-line
   }, []);
 
-  const handleChange = async (event) => {};
-  const handleSubmit = async (event) => {};
+  const handleChange = async (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: { card: elements.getElement(CardElement) },
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      setTimeout(() => {
+        clearCart();
+        navigate("/");
+      }, 10000);
+    }
+  };
 
   return (
     <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser && myUser.name} </h4>
+          <p>Your total is {formatPrice(shipping_fee + total_amount)} </p>
+          <p>Test Card Number: 4242 4242 4242 4242</p>
+        </article>
+      )}
       <form id="payment-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
--- src/context/user_context.js
+++ src/context/user_context.js
@@ -4,7 +4,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
   const { loginWithRedirect, logout, user } = useAuth0();
-  console.log(user);
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
